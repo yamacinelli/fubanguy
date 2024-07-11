b1550fce19a0a84214ee3a3d78e6bd534e879d3a
MarcioJCarvalho <marciojosedecarvalho@gmail.com>
2024-07-11 00:47:18 -0300

    clean code, poo, clean archteture and solid

--- src/application/use_cases/get_fighter_details.py
+++ src/application/use_cases/get_fighter_details.py
@@ -1,14 +1,40 @@
+"""
+This module provides functionality to create Fighter instances based on predefined data.
+
+The module imports fighter data from `data.fighter_assets` and defines a function
+to create Fighter instances using this data.
+
+Functions:
+    execute(fighter_name: str) -> Fighter: 
+        Creates and returns a Fighter instance based on the provided fighter name.
+"""
+
+from data.fighter_assets import FIGHTERS_DATA
 from domain.entities.fighter import Fighter
 
 
-def execute(
-    name: str, health: int, position: tuple, velocity: tuple, attack_power: tuple
-) -> Fighter:
+def execute(fighter_name: str) -> Fighter:
+    """
+    Creates and returns a Fighter instance based on the provided fighter name.
+
+    Args:
+        fighter_name (str): The name of the fighter to create.
+
+    Returns:
+        Fighter: An instance of the Fighter class with attributes loaded from FIGHTERS_DATA.
+
+    Raises:
+        ValueError: If no fighter with the given name is found in FIGHTERS_DATA.
+    """
+    fighter_data = FIGHTERS_DATA.get(fighter_name)
+
+    if not fighter_data:
+        raise ValueError(f"No fighter found with name: {fighter_name}")
+
     fighter = Fighter(
-        name=name,
-        health=health,
-        position=position,
-        velocity=velocity,
-        attack_power=attack_power,
+        name=fighter_data["name"],
+        health=fighter_data["health"],
+        position=fighter_data["position"],
+        attack_power=fighter_data["attack_power"],
     )
     return fighter
