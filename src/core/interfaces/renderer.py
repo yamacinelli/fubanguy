105cbd515fb9537d77dbd50132fedb8f279dafdd
MarcioJCarvalho <marciojosedecarvalho@gmail.com>
2024-07-14 02:06:39 -0300

    feat: [restructure] implements health bar

---
+++ src/core/interfaces/renderer.py
@@ -0,0 +1,32 @@
+"""
+This module contains the abstract base class for renderers.
+"""
+
+from abc import ABC, abstractmethod
+from typing import Tuple
+
+
+class Renderer(ABC):
+    """
+    An abstract base class for rendering shapes on a Pygame screen.
+
+    This class defines the interface that all renderers must implement.
+    """
+
+    @abstractmethod
+    def draw(
+        self,
+        color: Tuple[int, int, int],
+        position: Tuple[int, int],
+        dimensions: Tuple[int, int],
+    ):
+        """
+        Draws a shape on the screen.
+
+        Args:
+            color (Tuple[int, int, int]): The color of the shape in RGB format.
+            position (Tuple[int, int]): The (x, y) coordinates of the shape's
+                top-left corner.
+            dimensions (Tuple[int, int]): The (width, height) dimensions of the shape.
+        """
+        raise NotImplementedError("Subclasses must implement draw method.")
