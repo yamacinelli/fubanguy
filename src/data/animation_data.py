425f005effa43be51a3ec244177aae59d610a768
MarcioJCarvalho <marciojosedecarvalho@gmail.com>
2024-10-19 22:56:31 -0300

    tela responsiva

--- src/data/animation_data.py
+++ src/data/animation_data.py
@@ -1,19 +1,24 @@
 from core.shared.vector_2 import Vector2
+import infra.game_config as GC
 
+heigth = GC.SCREENSIZEHEIGHT / 100 * 47.05
+width = heigth / 100 * 62.5
+fighter_size_y = heigth * 0.4
+vertical_offset = fighter_size_y / 100 * 40
 
 ANIMATION_DATA = [
     {
         "name": "idle",
         "sprites": [
-            {"coordinate": Vector2(0, 40), "speed_animation": 0.8},
+            {"coordinate": Vector2(0, vertical_offset), "speed_animation": 0.8},
         ],
     },
     {
         "name": "walk",
         "sprites": [
-            {"coordinate": Vector2(0, 40), "speed_animation": 0.1},
-            {"coordinate": Vector2(100, 40), "speed_animation": 0.1},
-            {"coordinate": Vector2(200, 40), "speed_animation": 0.1},
+            {"coordinate": Vector2(0, vertical_offset), "speed_animation": 0.1},
+            {"coordinate": Vector2(width, vertical_offset), "speed_animation": 0.1},
+            {"coordinate": Vector2(width * 2, vertical_offset), "speed_animation": 0.1},
             # {"coordinate": Vector2(180, 0), "speed_animation": 0.1},
             # {"coordinate": Vector2(240, 0), "speed_animation": 0.1},
         ],
@@ -22,7 +27,7 @@ ANIMATION_DATA = [
         "name": "attack",
         "sprites": [
             # {"coordinate": Vector2(0, 240), "speed_animation": 0.1},
-            {"coordinate": Vector2(100, 240), "speed_animation": 0.1},
+            {"coordinate": Vector2(width, heigth + vertical_offset), "speed_animation": 0.1},
             # {"coordinate": Vector2(600, 0), "speed_animation": 0.2},
             # {"coordinate": Vector2(600, 0), "speed_animation": 0.2},
             # {"coordinate": Vector2(600, 0), "speed_animation": 0.2},
@@ -31,8 +36,8 @@ ANIMATION_DATA = [
     {
         "name": "jump",
         "sprites": [
-            {"coordinate": Vector2(0, 440), "speed_animation": 0.3},
-            {"coordinate": Vector2(100, 440), "speed_animation": 1.7},
+            {"coordinate": Vector2(0, heigth * 2 + vertical_offset), "speed_animation": 0.3},
+            {"coordinate": Vector2(width, heigth * 2 + vertical_offset), "speed_animation": 1.7},
             # {"coordinate": Vector2(2, 3), "speed_animation": 0.15},
             # {"coordinate": Vector2(3, 3), "speed_animation": 0.15},
             # {"coordinate": Vector2(4, 3), "speed_animation": 0.15},
@@ -42,7 +47,7 @@ ANIMATION_DATA = [
         "name": "block",
         "sprites": [
             # {"coordinate": Vector2(0, 440), "speed_animation": 0.3},
-            {"coordinate": Vector2(100, 440), "speed_animation": 0.1},
+            {"coordinate": Vector2(width, heigth * 2 + vertical_offset), "speed_animation": 0.1},
             # {"coordinate": Vector2(2, 3), "speed_animation": 0.15},
             # {"coordinate": Vector2(3, 3), "speed_animation": 0.15},
             # {"coordinate": Vector2(4, 3), "speed_animation": 0.15},
